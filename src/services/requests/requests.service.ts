d61763c68effc31773431b7716b0611b602eba30
MDz1985 <m_d@mail.uk>
2024-02-08 20:48:10 +0100

    fix: replace mocks with real data from request body

--- src/services/requests/requests.service.ts
+++ src/services/requests/requests.service.ts
@@ -3,16 +3,12 @@ import { PATH } from '../../models/server/enums/path';
 import { METHODS } from '../../models/server/enums/methods';
 import { UsersService } from '../users/users.service';
 import { IUser } from '../../models/users/user';
+import { once } from 'node:events';
 
-const newUserMock = {
-  id: '',
-  username: 'uuuuuuu',
-  age: 37,
-  hobbies: ['sds','sfs']
-}
 export class RequestsService {
   static instance: RequestsService;
   private _usersService = new UsersService();
+
   constructor() {
     return RequestsService.instance ?? (RequestsService.instance = this);
   }
@@ -28,37 +24,39 @@ export class RequestsService {
       res.writeHead(500, 'Error');
       res.end('error');
     }
-  }
+  };
 
   public async requestSeparator(req: IncomingMessage) {
     const url = req.url?.split('/');
     if (!url || !url[1] || !url[2] || url[2] !== PATH.USER || url.length > 4) {
-      this.handleErrors()
+      this.handleErrors();
     } else {
-      const [,,, id] = url;
-      const data = this.getRequestData();
+      const [, , , id] = url;
+      let data;
       switch (req.method) {
         case METHODS.GET:
           return this._usersService.getUsersRequest(id);
         case METHODS.POST:
-          return id ? this.handleErrors() : this._usersService.createUserRequest(data)
+          data = await this.getRequestData(req);
+          return id ? this.handleErrors() : this._usersService.createUserRequest(data);
         case METHODS.PUT:
-          return id ? this._usersService.updateUserRequest({...newUserMock, id}) : this.handleErrors()
+          data = await this.getRequestData(req);
+          return id ? this._usersService.updateUserRequest({ ...data, id }) : this.handleErrors();
         case METHODS.DELETE:
-          return id ? this._usersService.deleteUserRequest(id) : this.handleErrors()
+          return id ? this._usersService.deleteUserRequest(id) : this.handleErrors();
         default:
-          this.handleErrors()
+          this.handleErrors();
       }
     }
   }
 
-  getRequestData():IUser{
-    return {
-      id: '',
-      username: 'fdgd',
-      age: 17,
-      hobbies: []
-    }
+  async getRequestData(req: IncomingMessage): Promise<IUser> {
+    let result = '';
+    req.on('data', (chunk) => result += chunk);
+    req.on('end', () => result);
+    await once(req, 'end')
+    console.log(result);
+    return JSON.parse(result)
   }
 
 
