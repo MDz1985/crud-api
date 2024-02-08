5ccd19af7d61dd71816c00d11dd12de9e657b094
MDz1985 <m_d@mail.uk>
2024-02-08 10:17:39 +0100

    feat: add ability to receive requests and send responses

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,24 @@
-import http from 'node:http';
+import { createServer } from 'node:http';
+// import { once } from 'node:events';
+import { IncomingMessage, ServerResponse } from 'http';
 
 const hostname = 'localhost';
 const port = 4000;
-const server = http.createServer((_, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+async function HttpHandler(request: IncomingMessage, response: ServerResponse) {
+  try {
+    console.log(request.url)
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json; charset=utf-8');
+    response.writeHead(200, 'fine');
+    response.end(JSON.stringify({url: request.url,message: 'Hello word!'}))
+  } catch (e) {
+    response.writeHead(500, 'Error');
+    response.end('error')
+  }
+}
+
+createServer(HttpHandler)
+  .listen(port, hostname)
+  .on('listening', () => console.log(`Server running at http://${ hostname }:${ port }/`));
+
